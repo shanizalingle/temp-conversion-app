b576f11644171cd6e2e9b8b2679a329f5fe7eb8a
Shaniza Lingle <shanizalingle@gmail.com>
2022-08-09 12:15:51 -0700

    fix display bug for window alert

--- js/script.js
+++ js/script.js
@@ -1,21 +1,15 @@
-
-const celcius = parseInt(prompt("enter celcius temp"));
-const farenheit = parseInt(prompt("enter farenheit temp"));
-
-function celToFar(celcius) {
+ function celToFar(celcius) {
   const cToF = (celcius * 9/5) + 32;
   return cToF;
 }   
 
 function farToCel(farenheit) {
-  const fToC = (farenheit - 32) * 5/9;
+  const fToC = (farenheit - 32) * .55555556;
   return fToC;
 }
 
-celToFar();
-farToCel();
+const celcius = parseFloat(prompt("enter celcius temp"));
+const farenheit = parseFloat(prompt("enter farenheit temp"));
 
-window.alert(celcius + " degrees ");
-
-window.alert(celcius + " degrees celcius in farenheit is " + cToF);
-window.alert(farenheit + " degrees farenheit in celcius is " + fToC);
+window.alert(celcius + ' degrees celcius in farenheit is: ' + celToFar(celcius));
+window.alert(farenheit + ' degrees farenheit in calcius is: ' + farToCel(farenheit));
