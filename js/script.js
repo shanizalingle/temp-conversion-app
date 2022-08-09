6c41fe95a5223567c888b616893e007045355c33
Shaniza Lingle <shanizalingle@gmail.com>
2022-08-09 10:49:49 -0700

    WIP: functions  celToFar and farToCel

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-function tempConvApp (c,f) {
-let cToF = (c * 9/5) + 32;
-let fToC = (f - 32) * 5/9;
-} 
 
+const celcius = parseInt(prompt("enter celcius temp"));
+const farenheit = parseInt(prompt("enter farenheit temp"));
 
+function celToFar(celcius) {
+  const cToF = (celcius * 9/5) + 32;
+  return cToF;
+}   
 
-const c = window.prompt("enter celcius temp");
-const f = window.prompt("enter farenheit temp");
+function farToCel(farenheit) {
+  const fToC = (farenheit - 32) * 5/9;
+  return fToC;
+}
 
-window.alert(c + " degrees celcius in farenheit is " + cToF);
-window.alert(f + " degrees farenheit in celcius is " + fToC);
+celToFar();
+farToCel();
+
+window.alert(celcius + " degrees ");
+
+window.alert(celcius + " degrees celcius in farenheit is " + cToF);
+window.alert(farenheit + " degrees farenheit in celcius is " + fToC);
